fd0b0e417dfe1fc8742dc6dab0770131267c716c
oshrim <oshrimaayan@gmail.com>
2016-12-01 15:51:26 +0200

    loggin

--- devopsbuddy/src/main/java/com/devopsbuddy/web/i18n/I18NService.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/web/i18n/I18NService.java
@@ -1,6 +1,8 @@
 package com.devopsbuddy.web.i18n;
 
 import org.apache.tomcat.jni.Local;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -13,11 +15,13 @@ import java.util.Locale;
  */
 @Service
 public class I18NService {
+    private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
 
    @Autowired
    private MessageSource messageSource;
 
     public String getMessage(String messageId){
+        LOG.info("Returning i18n text for messageId {}",messageId);
         Locale locale = LocaleContextHolder.getLocale();
         return getMessage(messageId,locale);
     }
--- devopsbuddy/src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ devopsbuddy/src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DevopsbuddyApplicationTests {
-	
+
 	@Autowired
 	private I18NService i18NService;
 
